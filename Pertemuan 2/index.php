1ba2e328946bcc0004ff4fe45338041518609854
mfgn26 <mfashagn@gmail.com>
2021-02-12 14:06:30 +0700

    Menyelesaikan pertemuan 2

---
+++ Pertemuan 2/index.php
@@ -0,0 +1,66 @@
+<?php 
+/*
+Heryani
+203040169
+https://github.com/Heryani00/pw2021_203040169.git
+Pertemuan 2 - 10 Februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+
+
+<?php 
+// Pertemuan 2 - PHP DASAR
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam MTML
+// 2. HTML di dalam PHP
+
+// Variable dan Tipe Data
+// Variable
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+//$nama = "Heryani";
+//echo "Halo, nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// . 
+// $nama_depan = "Her";
+// $nama_belakang = "yani";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment
+// =, +=, -=, *=, /=, %=, .=, !=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Her";
+// $nama .= " ";
+// $nama .= "yani";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >+, ==
+// var_dump(1 == "1");
+
+// identitas 
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+$x = 30;
+var_dump($x < 20 || $x % 2 == 0);
+?>
