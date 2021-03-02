c24f14a3fe6e49987963d30be47409578241384a
Heryani00 <heryanishyt1105@gmail.com>
2021-03-02 19:34:29 +0700

    menyelesaikan tugas1 praktikum

---
+++ praktikum/tugas1/latihan1a.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+Heryani
+203040169
+Jum'at 13.00
+*/
+?>
+
+<?php 
+
+for ($i = 1; $i <= 3; $i++) {
+    for( $j = 1; $j <= 3; $j++ ) {
+        echo "Ini perulangan ke ($i, $j) <br>";
+    }
+
+}
+    
+
+?>
